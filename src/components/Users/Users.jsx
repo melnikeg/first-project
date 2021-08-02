c04dbf3c42dd0eece91b1bbdc422d5c372df1016
Egor <melnikeg@ukr.net>
2021-08-03 00:03:31 +0300

    Change Users.jsx, add user.png

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import styles from './users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
-                followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
-                followed: true, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Mariupol', country: 'Ukraine' }
-            },
-            {
-                id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
-                followed: false, fullName: 'Vova', status: 'I am a boss too', location: { city: 'Mangush', country: 'Ukraine' }
-            }
-        ]);
+    if (props.users.length === 0) {        
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
+
+        // props.setUsers([
+        //     {
+        //         id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
+        //         followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' }
+        //     },
+        //     {
+        //         id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
+        //         followed: true, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Mariupol', country: 'Ukraine' }
+        //     },
+        //     {
+        //         id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg',
+        //         followed: false, fullName: 'Vova', status: 'I am a boss too', location: { city: 'Mangush', country: 'Ukraine' }
+        //     }
+        // ]);
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed
@@ -36,12 +42,12 @@ const Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.city}</div>
-                        <div>{u.location.country}</div>
+                        <div>{"u.location.city"}</div>
+                        <div>{"u.location.country"}</div>
                     </span>
                 </span>
             </div>)
