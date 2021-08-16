b79b43aec43894b6521836c4e30aad6b93774f35
Egor <melnikeg@ukr.net>
2021-08-16 22:31:11 +0300

    Upgrade Users.js: pagination

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,16 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: [
-        /* { id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', 
-            followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-        { id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', 
-            followed: true, fullName: 'Andrew', status: 'I am a boss too', location: { city: 'Mariupol', country: 'Ukraine' } },
-        { id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', 
-            followed: false, fullName: 'Vova', status: 'I am a boss too', location: { city: 'Mangush', country: 'Ukraine' } }
-     */],
+    users: [],
+    pageSize: 100,
+    totalUsersCount: 0,
+    currentPage: 5
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -37,9 +35,15 @@ const usersReducer = (state = initialState, action) => {
                     })
             }
         case SET_USERS: {
-            return {
-                ...state, users: [...state.users, ...action.users]
-            }
+            return {...state, users: action.users}
+        }
+
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage }
+        }
+
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count }
         }
 
         default:
@@ -50,6 +54,8 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
 
 
 export default usersReducer;
